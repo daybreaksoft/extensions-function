f106044015e250bbbdf793ceb0a11b68e24df6bc
sid zhao <Sid@WEBEZI-SIsid.zhao@webezi.com.auD>
2018-11-06 13:58:15 +0800

    add exception extensions

---
+++ src/Daybreaksoft.Extensions.Functions/ExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Daybreaksoft.Extensions.Functions
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Get root message if there are inner exception
+        /// </summary>
+        public static string GetRootMessage(this Exception exception)
+        {
+            return GetInnerExceptionMessage(exception);
+        }
+
+        private static string GetInnerExceptionMessage(Exception exception)
+        {
+            return exception.InnerException != null ? GetInnerExceptionMessage(exception.InnerException) : exception.Message;
+        }
+    }
+}
---
+++ test/Daybreaksoft.Extensions.Functions.Tests/ExceptionExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using Daybreaksoft.Extensions.Functions.Tests.Requires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Daybreaksoft.Extensions.Functions.Tests
+{
+    public class ExceptionExtensionsTest
+    {
+        [Fact]
+        public void TestGetRootMessage()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception e)
+            {
+                Assert.Equal("Throw method 2", e.GetRootMessage());
+            }
+        }
+
+        private void Method1()
+        {
+            try
+            {
+                Method2();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Throw method 1", e);
+            }
+        }
+
+        private void Method2()
+        {
+            throw new Exception("Throw method 2");
+        }
+    }
+}
